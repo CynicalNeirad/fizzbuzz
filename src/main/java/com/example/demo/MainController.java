5c4cdf4c16ed95b181b39095227368ccb3312936
Richard Spencer <rlspenc15@earlham.edu>
2018-01-25 14:08:40 -0500

    Added range control Added proper response to the cycle number fixed output Anded multiplyer identification

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.DocFlavor;
+import java.util.Scanner;
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        String outputString = " ";
+        Integer multiplesCount = 0;
+        Scanner rangeInput = new Scanner(System.in);
+        System.out.println("Please Input your desired range.");
+        int range = rangeInput.nextInt();
+        for (int n = 1; n <= range; n++) {
+            outputString = outputString + " ";
+            if (n % 7 == 0 || n % 17 == 0 || n % 23 == 0) {
+                multiplesCount++;
+            }if (n % 3 == 0) {
+                outputString = outputString + "Fizz";
+            }if (n % 5 == 0) {
+                outputString = outputString + "Buzz";
+            }if (n % 3 != 0 && n % 5 != 0 ) {
+                outputString = outputString + n;
+            }
+        }
+        return (outputString + " And there are " + multiplesCount + " multiples of 7, 17, and 23.");
+
+
+    }
+}
